4019b7e5577cc99c1ad781a1158cf13ce5ab1217
Tibor Kircsi <tkircsi@gmail.com>
2022-10-01 20:59:21 +0200

    add job launch func

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/tkircsi/k8sjob
 
 go 1.18
 
-require k8s.io/client-go v0.25.2
+require (
+	k8s.io/api v0.25.2
+	k8s.io/apimachinery v0.25.2
+	k8s.io/client-go v0.25.2
+)
 
 require (
 	github.com/PuerkitoBio/purell v1.1.1 // indirect
@@ -36,8 +40,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.25.2 // indirect
-	k8s.io/apimachinery v0.25.2 // indirect
 	k8s.io/klog/v2 v2.70.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20220803162953-67bda5d908f1 // indirect
 	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+
+	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	jsonserializer "k8s.io/apimachinery/pkg/runtime/serializer/json"
 )
 
 func main() {
@@ -19,10 +27,11 @@ func main() {
 
 	flag.Parse()
 
-	fmt.Printf("Args: %s %s %s %s", *jobName, *containerImage, *entryCommand, *kubeConfig)
+	log.Printf("Args: %s %s %s %s\n", *jobName, *containerImage, *entryCommand, *kubeConfig)
 
 	clientset := connectToK8s(*kubeConfig)
-	fmt.Printf("%v\n", clientset)
+	launchK8sJob(clientset, jobName, containerImage, entryCommand)
+	log.Println("job successfully created")
 }
 
 func connectToK8s(kubeConfig string) *kubernetes.Clientset {
@@ -43,4 +52,58 @@ func connectToK8s(kubeConfig string) *kubernetes.Clientset {
 	}
 
 	return clientset
+}
+
+func launchK8sJob(clientset *kubernetes.Clientset, jobName, image, cmd *string) {
+	jobs := clientset.BatchV1().Jobs("default")
+	var backOffLimit int32 = 0
+
+	jobSpec := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      *jobName,
+			Namespace: "default",
+		},
+		Spec: batchv1.JobSpec{
+			Template: v1.PodTemplateSpec{
+				Spec: v1.PodSpec{
+					Containers: []v1.Container{
+						{
+							Name:    *jobName,
+							Image:   *image,
+							Command: strings.Split(*cmd, " "),
+						},
+					},
+					RestartPolicy: v1.RestartPolicyNever,
+				},
+			},
+			BackoffLimit: &backOffLimit,
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	job, err := jobs.Create(ctx, jobSpec, metav1.CreateOptions{})
+	if err != nil {
+
+		log.Fatalf("failed to create job: %q\n", *jobName)
+	}
+	cancel()
+
+	// Serializer = Decoder + Encoder.
+	serializer := jsonserializer.NewSerializerWithOptions(
+		nil,
+		nil,
+		nil,
+		jsonserializer.SerializerOptions{
+			Yaml:   true,
+			Pretty: false,
+			Strict: false,
+		},
+	)
+
+	yaml, err := runtime.Encode(serializer, job)
+	if err != nil {
+		log.Fatalln("can not serialize job into yaml")
+	}
+
+	log.Println(string(yaml))
 }
